1223c6a83a3ecea590635fb04924d3de1d03fd95
Vipul-26 <vipulsinghssm1@gmail.com>
2022-02-25 12:50:33 +0530

    Footer Component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Footer from "components/Footer/footer";
 import Header from "../components/Header/header";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
