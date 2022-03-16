cd258507f40c7457a9465701cf3983cea5da4ed8
Vipul-26 <vipulsinghssm1@gmail.com>
2022-03-16 07:16:53 +0530

    Added console type animation to main title

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classnames from 'classnames';
 import styles from './hero.module.css';
 
 const Hero = () => {
+
+  useEffect(() => {
+    function animateText(words, id, colors) {
+      var letterCount = 1;
+      var x = 1;
+      var waiting = false;
+      var target = document.getElementById(id)
+      target.setAttribute('style', 'color:' + colors[0])
+      window.setInterval(function () {
+        if (letterCount === 0 && waiting === false) {
+          waiting = true;
+          target.innerHTML = words[0].substring(0, letterCount)
+          window.setTimeout(function () {
+            var usedColor = colors.shift();
+            colors.push(usedColor);
+            var usedWord = words.shift();
+            words.push(usedWord);
+            x = 1;
+            target.setAttribute('style', 'color:' + colors[0])
+            letterCount += x;
+            waiting = false;
+          }, 1000)
+        } else if (letterCount === words[0].length + 1 && waiting === false) {
+          waiting = true;
+          window.setTimeout(function () {
+            x = -1;
+            letterCount += x;
+            waiting = false;
+          }, 1000)
+        } else if (waiting === false) {
+          target.innerHTML = words[0].substring(0, letterCount);
+          letterCount += x;
+        }
+      }, 120);
+    };
+    animateText(['Vipul Singh', 'UI Developer', 'Python Developer'], 'text', ['aliceblue', 'ghostwhite', 'lightblue'])
+  }, []);
+
   return (
     <section id="hero" className={styles.section}>
       <h1 className={styles.overline}>
         Hi there, my name is
       </h1>
-      <h2 className={styles.title}>
-        Vipul Singh.
-      </h2>
+      <div className={styles.container}>
+        <span id='text'></span>
+      </div>
       <h3 className={classnames(styles.subtitle, 'mt-3')}>
         I write code for responsive websites.
       </h3>
