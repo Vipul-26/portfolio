60f8ebb0f7302c253c904e2712174dcd7c401b37
Vipul-26 <vipulsinghssm1@gmail.com>
2022-09-21 14:12:25 +0530

    updated meta data

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -25,13 +25,16 @@ const Layout = ({ children }) => {
                     name="viewport"
                     content="width=device-width,initial-scale=1.0"
                 />
-                <meta name="description" content="A portfolio website created using nextjs & tailwindcss." />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+                <meta name="description" content="A portfolio website created using next-js & tailwindcss." />
                 <meta name="author" content="Vipul Singh" />
+                <meta name="keywords" content="portfolio" />
+                <meta name="theme-color" content="#0a192f" />
                 <meta property="og:title" content="Vipul's Resume" />
                 <meta property="og:description" content="This is a portfolio website" />
-                <meta name="keywords" content="portfolio" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://portfolio-vipul-26.vercel.app/" />
+                <meta name="twitter:image" content="https://portfolio-vipul-26.vercel.app/_next/image?url=%2FVipul.jpg&w=384&q=75" />
                 <meta name="robots" content="index, follow" />
             </Head>
             <Header />
